22f7b96ad6a4b712f2a88944c1ba989482ba631d
NicoAnguino <nikolas_28_95@hotmail.com>
2024-05-02 15:55:03 -0300

    Inicio de carga de ejercicio y cambios de estilos

--- EzpeletaNetCore8/Controllers/EjerciciosFisicosController.cs
+++ EzpeletaNetCore8/Controllers/EjerciciosFisicosController.cs
@@ -2,11 +2,47 @@ using Microsoft.AspNetCore.Mvc;
 using EzpeletaNetCore8.Models;
 using EzpeletaNetCore8.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EzpeletaNetCore8.Controllers;
 
 [Authorize]
 public class EjerciciosFisicosController : Controller 
 {
+     private ApplicationDbContext _context;
 
+    //CONSTRUCTOR
+    public EjerciciosFisicosController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        // Crear una lista de SelectListItem que incluya el elemento adicional
+        var selectListItems = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "0", Text = "[SELECCIONE...]" }
+        };
+
+        // Obtener todas las opciones del enum
+        var enumValues = Enum.GetValues(typeof(EstadoEmocional)).Cast<EstadoEmocional>();
+
+        // Convertir las opciones del enum en SelectListItem
+        selectListItems.AddRange(enumValues.Select(e => new SelectListItem
+        {
+            Value = e.GetHashCode().ToString(),
+            Text = e.ToString().ToUpper()
+        }));
+
+        // Pasar la lista de opciones al modelo de la vista
+        ViewBag.EstadoEmocionalInicio = selectListItems.OrderBy(t => t.Text).ToList();
+        ViewBag.EstadoEmocionalFin = selectListItems.OrderBy(t => t.Text).ToList();
+
+        var tipoEjercicios = _context.TipoEjercicios.ToList();
+        tipoEjercicios.Add(new TipoEjercicio{TipoEjercicioID = 0, Descripcion = "[SELECCIONE...]"});
+        ViewBag.TipoEjercicioID = new SelectList(tipoEjercicios.OrderBy(c => c.Descripcion), "TipoEjercicioID", "Descripcion");
+
+        return View();
+    }
 }
