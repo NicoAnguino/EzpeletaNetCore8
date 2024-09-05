7acfd601f0126c41e2c480ba852c5f869e7ff37c
Nicolas Anguino <nikolas_28_95@hotmail.com>
2024-09-04 21:55:08 -0300

    Vista parcial de modal

--- EzpeletaNetCore8/Controllers/EjerciciosFisicosController.cs
+++ EzpeletaNetCore8/Controllers/EjerciciosFisicosController.cs
@@ -21,6 +21,35 @@ public class EjerciciosFisicosController : Controller
 
     public IActionResult EjerciciosPorTipo()
     {
+          // Crear una lista de SelectListItem que incluya el elemento adicional
+        var selectListItems = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "0", Text = "[SELECCIONE...]" }
+        };
+
+        // Obtener todas las opciones del enum
+        var enumValues = Enum.GetValues(typeof(EstadoEmocional)).Cast<EstadoEmocional>();
+
+        // Convertir las opciones del enum en SelectListItem
+        selectListItems.AddRange(enumValues.Select(e => new SelectListItem
+        {
+            Value = e.GetHashCode().ToString(),
+            Text = e.ToString().ToUpper()
+        }));
+
+        // Pasar la lista de opciones al modelo de la vista
+        ViewBag.EstadoEmocionalInicio = selectListItems.OrderBy(t => t.Text).ToList();
+        ViewBag.EstadoEmocionalFin = selectListItems.OrderBy(t => t.Text).ToList();
+
+        var tipoEjercicios = _context.TipoEjercicios.ToList();
+        var tiposEjerciciosBuscar = tipoEjercicios.ToList();
+
+        tipoEjercicios.Add(new TipoEjercicio { TipoEjercicioID = 0, Descripcion = "[SELECCIONE...]" });
+        ViewBag.TipoEjercicioID = new SelectList(tipoEjercicios.OrderBy(c => c.Descripcion), "TipoEjercicioID", "Descripcion");
+
+        tiposEjerciciosBuscar.Add(new TipoEjercicio { TipoEjercicioID = 0, Descripcion = "[TODOS]" });
+        ViewBag.TipoEjercicioBuscarID = new SelectList(tiposEjerciciosBuscar.OrderBy(c => c.Descripcion), "TipoEjercicioID", "Descripcion");
+
         return View();
     }
 
